78c8a2efc088e1f8c28988a9cc46c589de7c6bc0
damlad12 <113645970+damlad12@users.noreply.github.com>
2023-08-30 21:23:13 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+namespace yeniproje
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+          
+            
+          
+            var form1 = new Form1();
+            Application.Run(form1);
+           
+           
+            
+        }
+    }
+}
